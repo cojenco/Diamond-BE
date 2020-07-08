16b048695eafedd221bc24e5ccc01f448ab0d31b
cojenco <icathyouyang@gmail.com>
2020-07-08 14:47:02 -0700

    hide secret_key and setup gitignore file

--- stones/diamondtrails/views.py
+++ stones/diamondtrails/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. This is diamond2trails")
+    return HttpResponse("Hello, world. This is diamond3trails")
--- stones/stones/settings.py
+++ stones/stones/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+from .secrets import *
 import os
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!q+5u-x*g(lj9@^it_dyo3!+eye6_m&95zqo7y-)z!+)jd-0fy'
+SECRET_KEY = DT3_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
