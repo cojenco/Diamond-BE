d036d00bae24b74f7ab651bcc30f5c995fd2a88c
cojenco <icathyouyang@gmail.com>
2020-07-08 22:46:06 -0700

    create index external API call and test out Django template views. able to render hard code results

--- stones/diamondtrails/views.py
+++ stones/diamondtrails/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from stones import secrets
+import requests
 # Create your views here.
 
+# def index(request):
+#     return HttpResponse("Hello, world. This is diamond3trails")
+
 def index(request):
-    return HttpResponse("Hello, world. This is diamond3trails")
+    url = 'https://www.hikingproject.com/data/get-trails'
+
+    payload = {
+        'key': secrets.HP_API_KEY,
+        'lat': '47.62',
+        'lon': '-122.19', 
+        'maxDistance': '20',
+    }
+
+    r = requests.get(url, params=payload).json()
+    # print(r)
+
+    context = {'trails_data': r}
+    # print(r.json())
+    # return HttpResponse("Hello, world. This is diamond3trails")
+    return render(request, 'diamondtrails/trails.html', context)
+
